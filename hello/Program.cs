c2e3c192b8b567ad2fa8fd1ff3c9c325ac8ba043
khuraibet <129894070+khuraibet@users.noreply.github.com>
2025-01-29 06:25:23 +0000

    Ask for a number and display it in console

--- hello/Program.cs
+++ hello/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a number: ");
+        var number = int.Parse(Console.ReadLine() ?? String.Empty);
+
+        Console.WriteLine("You entered the number " + number);
     }
 }
